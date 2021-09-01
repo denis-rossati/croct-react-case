b8a3e783cc21423c42952deca8367dafd131766e
denis-rossati <denis.rossati@hotmail.com>
2021-09-01 17:45:58 -0300

    refactoring login page

--- react-case-challenge/src/pages/LoginPage.jsx
+++ react-case-challenge/src/pages/LoginPage.jsx
@@ -5,12 +5,11 @@ export default function LoginPage () {
   const [validEmail, setValidEmail] = useState(false)
   const [buttonDisabled, setButtonDisabled] = useState(true)
 
-  const willEnableButton = () => {
-    validEmail && validPassword ? setButtonDisabled(false) : setButtonDisabled(true)
-  }
+  const willEnableButton = () => validEmail && validPassword ? setButtonDisabled(false) : setButtonDisabled(true)
 
   const verifyEmail = ({ target: { value } }) => {
-    const emailRegex = /[\w\S^0-9]@[\w\S^0-9].com/gm
+    // I will keep the regex very simple (and insecure, I know) just this time XD
+    const emailRegex = /[\w\S]+@[\w\S]+.com/gm
     if (emailRegex.test(value)) {
       setValidEmail(true)
     } else {
