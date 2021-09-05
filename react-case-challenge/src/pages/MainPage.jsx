e4bcc210499dca1bfdaf3ecd03f4b26f408699be
denis-rossati <denis.rossati@hotmail.com>
2021-09-05 00:03:41 -0300

    added useEffect to fetch onload

--- react-case-challenge/src/pages/MainPage.jsx
+++ react-case-challenge/src/pages/MainPage.jsx
@@ -1,12 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import MealGrid from '../components/MealGrid';
 import SearchBar from '../components/SearchBar';
 
 export default function MainPage() {
   const [meals, setMeals] = useState([]);
+
+  const setMealByResult = async (value) => {
+    const query = `www.themealdb.com/api/json/v1/1/search.php?s=${value}`;
+    const request = await fetch(query);
+    const response = await request.json();
+    setMeals(response);
+  };
+
+  useEffect(async () => {
+    const query = 'www.themealdb.com/api/json/v1/1/randomselection.php';
+    const request = await fetch(query);
+    const response = await request.json();
+    setMeals(response);
+  }, []);
+
   return (
     <div>
-      <SearchBar />
+      <SearchBar searchFunction={setMealByResult} />
       <MealGrid mealResult={meals} />
     </div>
   );
