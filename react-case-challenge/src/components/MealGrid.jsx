87419511d2b7b0dff92a1237be1a8d9a7b2fc628
denis-rossati <denis.rossati@hotmail.com>
2021-09-05 17:33:11 -0300

    grade of meals completed

--- react-case-challenge/src/components/MealGrid.jsx
+++ react-case-challenge/src/components/MealGrid.jsx
@@ -1,21 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-export default function MealGrid({ mealResult: { meals } }) 
-  
+export default function MealGrid({ mealResult }) {
+  const mealsMap = (arrMeals) => arrMeals.map(({
+    idMeal, strMealThumb, strMeal, strArea,
+  }) => (
+    <div key={idMeal}>
+      <Link to={`/${idMeal}`}>
+        <img src={strMealThumb} alt="Recipe after been cooked" />
+        <p>{strMeal}</p>
+        <p>{ strArea }</p>
+      </Link>
+    </div>
+  ));
+
   const renderMeals = () => {
+    const { meals } = mealResult;
     if (Array.isArray(meals)) {
       return mealsMap(meals);
     }
+    return 'Loading recipes...';
   };
-  console.log(meals);
+  console.log(mealResult);
+
   return (
-    <div>
+    <section>
       { renderMeals() }
-    </div>
+    </section>
   );
 }
 
 MealGrid.propTypes = {
-  mealResult: PropTypes.arrayOf(PropTypes.object).isRequired,
+  mealResult: PropTypes.objectOf(PropTypes.array).isRequired,
 };
