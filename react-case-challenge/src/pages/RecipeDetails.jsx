662f58828a4817bcaa426dcbf113bada61236a9d
denis-rossati <denis.rossati@hotmail.com>
2021-09-06 13:36:59 -0300

    creating useEffect to fetch API

--- react-case-challenge/src/pages/RecipeDetails.jsx
+++ react-case-challenge/src/pages/RecipeDetails.jsx
@@ -1,16 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router';
 
 export default function RecipeDetails() {
   // eslint-disable-next-line no-unused-vars
   const [isLoading, setIsLoading] = useState(true);
+  const [mealContent, setMealContent] = useState([]);
 
-  const renderLoading = () => (isLoading
+  const renderContent = () => (isLoading
     ? (<p>Loading...</p>) : <></>);
+
+  useEffect(() => {
+    const fetchingMeal = () => {
+      const { id } = useParams();
+      const url = `www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+      fetch(url)
+        .then((response) => response.json())
+        .then(({ meals }) => {
+          setIsLoading(false);
+          setMealContent(meals);
+        })
+        .catch(() => setIsLoading(true));
+    };
+    fetchingMeal();
+  }, []);
 
   return (
     <div>
       {
-        renderLoading()
+        renderContent()
       }
     </div>
   );
