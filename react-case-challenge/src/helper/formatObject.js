e5c88da54c9ed35caca6697009f4225c99a9bdd1
denis-rossati <denis.rossati@hotmail.com>
2021-09-06 14:52:04 -0300

    fixing logic on helper

--- react-case-challenge/src/helper/formatObject.js
+++ react-case-challenge/src/helper/formatObject.js
@@ -1,10 +1,10 @@
 // The API send some key object empty, this function will remove it.
 
 const formatObject = (obj) => Object.keys(obj).reduce((acc, el) => {
-  if (obj[el] !== '' || !!(obj[el]) !== false) {
-    acc[el] = obj[el];
+  if (obj[el] === '' || obj[el] === ' ' || !!(obj[el]) === false) {
     return acc;
   }
+  acc[el] = obj[el];
   return acc;
 }, {});
 
