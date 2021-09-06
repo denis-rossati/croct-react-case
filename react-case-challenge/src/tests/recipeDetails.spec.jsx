ee37267e7a9d0290f15ab5cf77ee48d20d70ea2f
denis-rossati <denis.rossati@hotmail.com>
2021-09-06 13:02:11 -0300

    adding history.push inthe tests

--- react-case-challenge/src/tests/recipeDetails.spec.jsx
+++ react-case-challenge/src/tests/recipeDetails.spec.jsx
@@ -2,8 +2,9 @@
 /* eslint-disable no-undef */
 import React from 'react';
 import renderWithRouter from './helper/renderWithRouter';
-import RecipesDetails from '../pages/RecipeDetails';
+import RecipeDetails from '../pages/RecipeDetails';
 
+jest.spyOn(global, 'fetch');
 global.fetch = jest.fn(() => Promise.resolve({
   json: () => Promise.resolve({
 
@@ -12,7 +13,8 @@ global.fetch = jest.fn(() => Promise.resolve({
 
 describe('testing recipeDetails page', () => {
   it('Shold start with loading text', () => {
-    const { getByText } = renderWithRouter(<RecipesDetails />);
+    const { getByText, history } = renderWithRouter(<RecipeDetails />);
+    history.push('/52977');
     const loadingText = getByText('Loading...');
     expect(loadingText).toBeDefined();
   });
