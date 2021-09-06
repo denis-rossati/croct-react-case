110f5e346ea769026aa5413be1863e4956a7071e
denis-rossati <denis.rossati@hotmail.com>
2021-09-06 13:01:52 -0300

    adding route for RecipeDetails

--- react-case-challenge/src/App.jsx
+++ react-case-challenge/src/App.jsx
@@ -7,16 +7,20 @@ import {
 
 import LoginPage from './pages/LoginPage';
 import MainPage from './pages/MainPage';
+import RecipeDetails from './pages/RecipeDetails';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/">
-          <LoginPage />
+        <Route exact path="/:id">
+          <RecipeDetails />
         </Route>
         <Route exact path="/main-page">
           <MainPage />
+        </Route>
+        <Route exact path="/">
+          <LoginPage />
         </Route>
       </Switch>
     </BrowserRouter>
