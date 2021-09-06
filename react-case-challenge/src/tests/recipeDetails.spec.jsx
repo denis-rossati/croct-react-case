4a4ae5ff994bba926f356f601d94d559e30e5d46
denis-rossati <denis.rossati@hotmail.com>
2021-09-06 13:27:16 -0300

    creating mock test for recipeDetails component

--- react-case-challenge/src/tests/recipeDetails.spec.jsx
+++ react-case-challenge/src/tests/recipeDetails.spec.jsx
@@ -3,19 +3,22 @@
 import React from 'react';
 import renderWithRouter from './helper/renderWithRouter';
 import RecipeDetails from '../pages/RecipeDetails';
+import mockApiResponse from './helper/apiResponseMock';
 
-jest.spyOn(global, 'fetch');
-global.fetch = jest.fn(() => Promise.resolve({
-  json: () => Promise.resolve({
-
-  }),
-}));
-
-describe('testing recipeDetails page', () => {
+describe('testing loading state', () => {
   it('Shold start with loading text', () => {
     const { getByText, history } = renderWithRouter(<RecipeDetails />);
     history.push('/52977');
     const loadingText = getByText('Loading...');
     expect(loadingText).toBeDefined();
+  });
+});
+
+describe('Testing page after backend have been throwed a response', () => {
+  it('Should treat and load all the information in the response', () => {
+    jest.spyOn(global, 'fetch');
+    global.fetch = jest.fn(() => Promise.resolve({
+      json: () => Promise.resolve(mockApiResponse),
+    }));
   });
 });
