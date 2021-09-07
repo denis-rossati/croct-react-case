b44caee1417d22095e00b51d3032d1c729bd4867
denis-rossati <denis.rossati@hotmail.com>
2021-09-07 16:46:18 -0300

    adding function to render recipes based on user interests

--- react-case-challenge/src/components/CustomGreetingMessage.jsx
+++ react-case-challenge/src/components/CustomGreetingMessage.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { useCroct } from '@croct/plug-react';
 
 export default function CustomGreetingMessage({ country, changeMealGrid }) {
+  const croct = useCroct();
   const [haveFindRecipe, setHaveFindedRecipe] = useState(true);
 
   const customCallbackMessage = (callback, message) => callback(message);
@@ -11,6 +13,18 @@ export default function CustomGreetingMessage({ country, changeMealGrid }) {
       .then((response) => response.json())
       .then((data) => changeMealGrid(data))
       .catch((err) => customCallbackMessage(console.error, err));
+  };
+
+  const renderInitialRecipes = async () => {
+    const isReturning = await croct.evaluate('user is returning');
+    if (isReturning) {
+      const interests = await croct.evaluate('user\'s interests');
+      const randomIndex = Math.round(Math.random() * (interests.length - 1));
+      const url = `www.themealdb.com/api/json/v1/1/filter.php?a=${interests[randomIndex]}`;
+      const response = await fetch(url);
+      const data = response.json;
+      changeMealGrid(data);
+    }
   };
 
   useEffect(() => {
