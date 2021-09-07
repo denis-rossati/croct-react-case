3ad37656471fef23133340ef9a86494b14528fb4
denis-rossati <denis.rossati@hotmail.com>
2021-09-07 16:35:11 -0300

    adding component to profile

--- react-case-challenge/src/pages/Profile.jsx
+++ react-case-challenge/src/pages/Profile.jsx
@@ -1,34 +1,54 @@
+/* eslint-disable react/no-array-index-key */
 import React, { useState, useEffect } from 'react';
 import { useCroct } from '@croct/plug-react';
+import RecipeSnippet from '../components/RecipeSnippet';
+import formatRecipes from '../helper/formatRecipes';
 
 export default function Profile() {
   const croct = useCroct();
+  // if I get employed, my first question is to know why does react and croct together could'nt
+  // accept components mounted inside a function :(
   const [email, setEmail] = useState('Loading...');
   const [areaInterests, setAreaInterests] = useState([]);
+  const [recipes, setRecipes] = useState([]);
 
   useEffect(async () => {
     const loadEmail = async () => {
-      await croct.evaluate('user\'s email').then((loadedEmail) => setEmail(loadedEmail));
+      await croct.evaluate('user\'s email')
+        .then((loadedEmail) => setEmail(loadedEmail));
     };
+
     const foodInterests = async () => {
-      const interests = await croct.evaluate('user\'s interests');
-      setAreaInterests(interests);
+      await croct.evaluate('user\'s interests')
+        .then((loadedInterests) => setAreaInterests(loadedInterests));
     };
-    await foodInterests();
+
+    const loadRecipes = async () => {
+      await croct.evaluate('user\' recipes')
+        .then((loadedRecipes) => formatRecipes(loadedRecipes))
+        .then((newRecipes) => setRecipes(newRecipes));
+    };
+
     await loadEmail();
+    await foodInterests();
+    await loadRecipes();
   }, []);
 
-  const renderInterests = () => {
-    const result = areaInterests
-      .reduce((acc, area) => {
-        if (acc.indexOf(area) === -1) {
-          acc.push(area);
-        }
-        return acc;
-      }, [])
-      .map((foodArea) => <li key={foodArea}>{foodArea}</li>);
-    return result;
-  };
+  const renderInterests = () => areaInterests
+    .reduce((acc, area) => {
+      if (acc.indexOf(area) === -1) {
+        acc.push(area);
+      }
+      return acc;
+    }, [])
+    .map((foodArea) => <li key={foodArea}>{foodArea}</li>);
+
+  const renderRecipes = () => recipes.map((meal, index) => (
+    <RecipeSnippet
+      key={index}
+      recipe={meal}
+    />
+  ));
 
   return (
     <div>
@@ -41,6 +61,9 @@ export default function Profile() {
       </ul>
 
       <p>As comidas em que você deu like foram:</p>
+      <div>
+        { renderRecipes() }
+      </div>
     </div>
   );
 }
